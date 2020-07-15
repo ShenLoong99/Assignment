6deece10671a9e717022f7331cb3203543c4bb1d
ShenLoong99 <yithuettsk29@gmail.com>
2020-07-15 21:14:33 +0800

    remove gitignore in client folder

---
+++ client/build/precache-manifest.9339cfa2d3c49db5b5776a6f6925e3c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566cebc39fa316c88c1a9b599766becf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d526beb30ef854e1925",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0d526beb30ef854e1925",
+    "url": "/static/js/2.5284486a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5284486a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c71c17ec2ab53ed39753",
+    "url": "/static/js/main.39026b65.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
